e3c2b61d46b6ed629720ce822d550daad692702d
kurt john <kurtjohn2112>
2022-02-09 20:32:40 +0800

    updated credentials

--- model/connection.php
+++ model/connection.php
@@ -1,12 +1,26 @@
 <?php 
+    // function connect_db(){
+
+
+    //     $conn = new mysqli(
+    //                         'localhost',
+    //                         'root',
+    //                         'root',
+    //                         'teacher_eval'
+    //                     );
+
+
+    //     return $conn;
+    // }
     function connect_db(){
 
+        // hosting credentials, do not touch this or enable this
 
         $conn = new mysqli(
-                            'localhost',
-                            'root',
-                            'root',
-                            'teacher_eval'
+                            'us-cdbr-east-05.cleardb.net',
+                            'b41d6bbe2d096e',
+                            '12fc5629',
+                            'heroku_f78abd310387182'
                         );
 
 
